83a790bb46ae05f9a004dae8bc84248145943d85
ellie3413 <145912699+ellie3413@users.noreply.github.com>
2025-04-04 07:22:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -91,6 +91,15 @@ html, body, div, span, input, label, textarea, button, section, article, aside, 
         color: #111;
         text-align: center
     }
+                
+    .catchline {
+        font-family: 'MaruBuri' !important;
+        font-size: 16px;
+        font-weight: 500;
+        color: #686D76;
+        text-align: center
+    }
+    
     div.stButton > button {
     font-family: 'MaruBuri' !important;
     background-color:#3D3B40;
@@ -131,8 +140,9 @@ div.stButton > button:hover {
             """, unsafe_allow_html=True)
 
         st.markdown('<h1 class="mango-title">  M$ney<span class="dot">.</span></h1>', unsafe_allow_html=True)
-        st.markdown('<div class="subtitle">임대차계약 전문가 Miney와 함께 떠나는 내 보증금 되찾기 여정</div>', unsafe_allow_html=True)
-        st.markdown('<div class="subtitle">Miney가 처음이라면 정확한 분석을 위해 튜토리얼을 확인해주세요!</div>', unsafe_allow_html=True)
+        st.markdown('<div class="subtitle">보증금, 더는 혼자 싸우지 마세요</div>', unsafe_allow_html=True)
+        st.markdown('<div class="subtitle">내만의 보증금 AI 지킴이: M$ney</div>', unsafe_allow_html=True)
+        st.markdown('<div class="catchline">M$ney가 처음이라면 정확한 분석을 위해 튜토리얼을 확인해주세요!</div>', unsafe_allow_html=True)
     
     # 오른쪽 버튼 - HTML 버튼 태그 사용
     with right:
