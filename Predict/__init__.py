702f5e2c592cfca942d6b6e2371f0b216206db5e
K0ykwon <yko081524@yonsei.ac.kr>
2025-04-05 03:14:12 +0900

    Update __init__.py

--- Predict/__init__.py
+++ Predict/__init__.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 def bertPredict(text):
     cleaned_text, enough = cleaner(text)
-    if(enough): return predict(cleaned_text)
-    return 0
+    if(enough): return str(predict(cleaned_text) * 100) + '%'
+    return "0"
